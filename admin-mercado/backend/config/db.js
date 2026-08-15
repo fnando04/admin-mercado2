6af1bc7999e039663e9767542a934de32edaa734
fnando04 <233111346@upmh.edu.mx>
2026-08-15 09:09:22 -0600

    Actualización, conexión DB railway

--- admin-mercado/backend/config/db.js
+++ admin-mercado/backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "31501",
-    database: "db_merc",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "31501",
+    database: process.env.DB_NAME || "db_merc",
     waitForConnections: true,
     connectionLimit: 10
 });
